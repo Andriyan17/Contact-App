969f3a836d712dca0dea9c18f7c5379c65c18d04
andriyan ginting <andrginting17@gmail.com>
2023-09-26 18:10:58 +0700

    Create `contactItemBody`

---
+++ src/ContactItemBody.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+function ContactItemBody({ name, tag }) {
+  <div className="contact-item__body">
+    <h3 className="contact-item__title">{name}</h3>
+    <p className="contact-item__username">{tag}</p>
+  </div>;
+}
+
+export default ContactItemBody;
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const element = <h1>Hello, world!</h1>;
+
+const root = createRoot(document.getElementById('root'));
+root.render(element);
