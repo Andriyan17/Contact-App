4860790e1e319f5ec7907116a4402b30db87d0ec
andriyan ginting <andrginting17@gmail.com>
2023-09-26 18:17:11 +0700

    Create `ContactItemImage`

---
+++ src/ContactItemImage.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+function ContactItemImage({ imageUrl }) {
+  <div className="contact-item__image">
+    <img src={imageUrl} alt="profile-image" />
+  </div>;
+}
+
+export default ContactItemImage;
