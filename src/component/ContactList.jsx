ff6fe774bad21dd926039626c70ad43557997427
andriyan ginting <andrginting17@gmail.com>
2023-09-28 19:52:58 +0700

    create eventhandler onDelete in `ContactApp.jsx`

--- src/component/ContactList.jsx
+++ src/component/ContactList.jsx
@@ -3,11 +3,16 @@
 import React from 'react';
 import ContactItem from './ContactItem';
 
-function ContactList({ contacts }) {
+function ContactList({ contacts, id, onDelete }) {
   return (
     <div className="contact-list">
       {contacts.map((contact) => (
-        <ContactItem key={contact.id} {...contact} />
+        <ContactItem
+          key={contact.id}
+          id={id}
+          onDelete={onDelete}
+          {...contact}
+        />
       ))}
     </div>
   );
